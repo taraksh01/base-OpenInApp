bcd501abcbe1af0d1fe9e8ca97abdaf41a32d66d
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-02-02 22:44:16 +0530

    Create side menu

---
+++ src/pages/Menu.js
@@ -0,0 +1,58 @@
+import BaseInverted from "../assets/baseInverted.svg";
+import Dashboard from "../assets/dashboard.svg";
+import Upload from "../assets/upload.svg";
+import Invoice from "../assets/invoice.svg";
+import Schedule from "../assets/schedule.svg";
+import Calender from "../assets/calender.svg";
+import Notification from "../assets/notification.svg";
+import Settings from "../assets/settings.svg";
+import Cross from "../assets/cross.svg";
+
+const Menu = ({ showMenu, setShowMenu }) => {
+  return (
+    <div className="md:w-52 w-72 font-semibold">
+      <div className="flex justify-between md:justify-center items-center gap-8 p-5">
+        <img src={BaseInverted} alt="Base" />
+        <h1 className="font-semibold text-xl">Base</h1>
+        <img
+          src={Cross}
+          alt="Cross"
+          className="md:hidden block"
+          onAbort={() => setShowMenu(!showMenu)}
+        />
+      </div>
+      <div className="flex flex-col gap-8 text-gray-400 p-5">
+        <div className="flex gap-4">
+          <img src={Dashboard} alt="Dashboard" />
+          <p>Dashboard</p>
+        </div>
+        <div className="flex gap-4">
+          <img src={Upload} alt="Upload" />
+          <p className="text-indigo-500">Upload</p>
+        </div>
+        <div className="flex gap-4">
+          <img src={Invoice} alt="Invoice" />
+          <p>Invoice</p>
+        </div>
+        <div className="flex gap-4">
+          <img src={Schedule} alt="Schedule" />
+          <p>Schedule</p>
+        </div>
+        <div className="flex gap-4">
+          <img src={Calender} alt="Calender" />
+          <p>Calender</p>
+        </div>
+        <div className="flex gap-4">
+          <img src={Notification} alt="Notification" />
+          <p>Notification</p>
+        </div>
+        <div className="flex gap-4">
+          <img src={Settings} alt="Settings" />
+          <p>Settings</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
