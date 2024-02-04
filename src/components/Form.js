f73ceff1a388426c4a96d8fa25420ff420e5875a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-02-04 09:21:16 +0530

    Added form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,38 @@
+import { useState } from "react";
 import Button from "./Button";
 import InputBox from "./Input";
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+  const login = () => {
+    if (email.match(/\S+@\S+\.\S+/) && password.length > 6) {
+      setError("");
+      navigate("/home");
+    } else setError("Email or password not valid");
+  };
+
   return (
     <form className="flex flex-col gap-6 p-5">
       <InputBox
         type={"email"}
         label={"Email Address"}
         placeholder={"your@email.com"}
+        setValue={setEmail}
       />
       <InputBox
         type={"password"}
         label={"Password"}
         placeholder={"Choose a strong password"}
+        setValue={setPassword}
       />
       <a className="text-blue-600 cursor-pointer">Forgot password?</a>
-      <Button type={"button"} text={"Sign In"} />
+      {error && <p className="text-red-600 text-center">{error}</p>}
+      <Button type={"button"} text={"Sign In"} click={login} />
     </form>
   );
 };
