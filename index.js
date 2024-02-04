444eff805d388f38b10afa1ebc1b00597bf17284
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-02-04 09:28:30 +0530

    Add routing for the app

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 import { createRoot } from "react-dom/client";
 import App from "./src/App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./src/pages/Login";
+import { Suspense, lazy } from "react";
 
-createRoot(document.getElementById("root")).render(<App />);
+const Home = lazy(() => import("./src/pages/Home"));
+
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "home",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={process.env.CLIENT_ID}>
+    <RouterProvider router={appRoute}>
+      <App />
+    </RouterProvider>
+  </GoogleOAuthProvider>
+);
