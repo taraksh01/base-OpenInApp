319b4dccd0474c6e884551ee529d7357b4bf1a48
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2024-02-04 09:20:32 +0530

    Add click function on button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,9 @@
-const Button = ({ type, text, img, property }) => {
+const Button = ({ type, text, img, property, click }) => {
   return (
     <button
       type={type}
       className={`text-white w-full bg-indigo-500 h-11 rounded-lg font-bold flex justify-center items-center flex-shrink-0 gap-3 ${property}`}
+      onClick={click}
     >
       {<img src={img} />} {text}
     </button>
